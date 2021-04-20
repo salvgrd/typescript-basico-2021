bc0204c880af1d5bc2e31767a53ce86406f101e3
salvgrd <salvgrd@gmail.com>
2021-04-21 00:38:41 +0200

    [feat] first class excersises

--- app.ts
+++ app.ts
@@ -1,3 +1,43 @@
-const helloWorld = 'Hola Mundo!'
+/** Ejercicio 1
+ * Calcular el area de un triangulo de base = 100 y altura == 300
+ */
 
-console.log(helloWorld)
+function calcularArea( base: number, altura: number ): number {
+    return (base * altura) / 2
+}
+
+const area: number = calcularArea(200, 300)
+console.log(`El area del triangulo es: ${area}`)
+
+/** Ejercicio 2
+ * Calcular la hipotenusa de un triangulo con lados A = 200 y B = 350
+ */
+
+function calcularHipotenusa( _catetoA: number, _catetoB: number ): number {
+    const sumaDeCuadrados: number = (_catetoA**2) + (_catetoB**2)
+    return Math.sqrt(sumaDeCuadrados)
+}
+const catetoA: number = 200
+const catetoB: number = 350
+
+const hipotenusa = calcularHipotenusa(catetoA, catetoB)
+
+console.log(`La hipotenusa del triangulo es: ${hipotenusa}`)
+
+/** Ejercicio 3
+ * Determinar si el triangulo anterior es equilatero, escaleno o isosceles
+ */
+
+function determinarTipo( _catetoA: number, _catetoB: number, _hipotenusa: number ): string {
+    if (_catetoA === _catetoB && _catetoA === _hipotenusa ) {
+        return "Equilatero"
+    } else if ( _catetoA === _catetoB || _catetoA === _hipotenusa || _catetoB === _hipotenusa ) {
+        return "Escaleno"
+    } else {
+        return "Isosceles"
+    }
+}
+
+const tipoDelTriangulo: string = determinarTipo(catetoA, catetoB, hipotenusa)
+
+console.log(`El tipo del triangulo es: ${tipoDelTriangulo}`)
