9627c7592dac6c9c07e4ac20f5934b49caca36ea
salvgrd <salvgrd@gmail.com>
2021-04-21 00:45:57 +0200

    [fix] removed excersise

--- app.ts
+++ app.ts
@@ -35,13 +35,3 @@ usuario['nombre'] = 'Pedrito'
 usuario.edad = 32
 
 console.log(`Ahora me llamo ${usuario.nombre} y tengo ${usuario.edad} años`)
-
-/**
- * Ejercicio
- * A partir de dos listas, una con productos de supermercado y otra con cantidades
- * crear un objeto "carrito" cuyas claves sean los productos, y los valores las cantidades
- */
-
-const productos = [ 'manzanas', 'peras', 'mangos' ]
-const cantidades = [ 10, 9, 14 ]
-let carrito = {}
