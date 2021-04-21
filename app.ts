b20bd67d7c250517c2d736f6ca787e718a4e4c2b
salvgrd <salvgrd@gmail.com>
2021-04-21 15:43:56 +0200

    [feat] more details

--- app.ts
+++ app.ts
@@ -6,12 +6,17 @@ let array: number[] = [ 1, 2, 3, 4 ]
 
 const arrayDeLoQueSea: any[] = [ 123, "strings", false, [ "hola", "mundo" ], 456 ]
 
+// destructuracion de una lista
+
+const [ number, string, booleano, arrayAnidado ] = arrayDeLoQueSea
+
 console.log('La longitud del array es:', array.length) // array.length devuelve la longitud de la lista
 
 // accediendo a las posiciones de una lista
 
 const posicion = 0
-console.log(`El elemento en la posicion ${posicion} es:`, array[posicion]) // array[posicion] accede a la posicion indicada en el array
+// array[posicion] accede a la posicion indicada en el array
+console.log(`El elemento en la posicion ${posicion} es:`, array[posicion]) 
 
 array[posicion] = 157
 
@@ -19,7 +24,7 @@ console.log(`Ahora el valor de la posicion ${posicion} es:`, array[posicion])
 
 const nuevosNumeros = [ 5, 6, 7, 8 ]
 
-console.log({ arrayConSpread: [ 2, 3, ...nuevosNumeros, 11, 12] })
+console.log({ arrayConSpread: [ 2, 3, ...nuevosNumeros, 11, 12] }) // Operador spread
 
 // Recorriendo un array
 
@@ -41,5 +46,8 @@ console.log(`Me llamo ${usuario.nombre} y tengo ${usuario['edad']} años de edad
 
 usuario['nombre'] = 'Pedrito'
 usuario.edad = 32
+
+// Destructuracion de un objeto
+const { nombre, edad } = usuario
 
 console.log(`Ahora me llamo ${usuario.nombre} y tengo ${usuario.edad} años`)
