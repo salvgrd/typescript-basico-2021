d8b4c50dee25a9629e4dda5f1575e87a8662d42b
salvgrd <salvgrd@gmail.com>
2021-04-21 17:14:31 +0200

    [feat] added answers

--- app.ts
+++ app.ts
@@ -4,20 +4,40 @@
  * crear un objeto "carrito" cuyas claves sean los productos, y los valores las cantidades
  */
 
+// Output/Salida: { manzanas: 10, peras: 9, cantidades: 14 }
+
  const productos = [ 'manzanas', 'peras', 'mangos' ]
  const cantidades = [ 10, 9, 14 ]
- let carrito = {}
+ const carrito = productos.reduce( (carrito, producto, idx) => {
+    carrito[producto] = cantidades[idx]
+    return carrito
+ } , {})
 
  console.log({ carrito })
 
 /** Ejercicio 2
- * Crear tres objetos/JSON/diccionarios con las propiedades "modelo": string y "velocidad_km_hora": number, que representen coches.
- * Determinar en cuanto tiempo recorrerán 1000 kilometros
+ * Crear tres objetos/JSON/diccionarios con las propiedades 
+ * "modelo": string y "velocidad_km_hora": number, que representen coches.
+ * Determinar en cuanto tiempo recorrerán 1000 kilometros cada uno
  */
 
-let coches = {}
-
+// Output/Salida: el coche X recorrerá la distancia Y en N horas
+const modelos = [ "Lambo Gallardo", "Maserati Antonio", "Opel Corsa" ]
+const kmh = [ 300, 350, 700 ]
 const distancia = 1000
+
+const coches = modelos.map( (modelo, idx) => {
+    const coche = {
+        modelo,
+        velocidad_km_hora: kmh[idx]
+    }
+    return coche
+})
+
+coches.forEach( coche => {
+    const tiempo = distancia / coche.velocidad_km_hora
+    console.log(`El ${coche.modelo} recorrera ${distancia} km en ${tiempo} horas`)
+})
 
 // console.log({ coches })
 
