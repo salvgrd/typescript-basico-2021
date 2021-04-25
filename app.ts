0dc478c3fcb95a390623cad9de1920e15b532963
salvgrd <salvgrd@gmail.com>
2021-04-26 01:56:41 +0200

    [feat] cake exercise

--- app.ts
+++ app.ts
@@ -1,58 +1,21 @@
-const helloWorld = 'Hola Mundo!'
-
-// Narrowing
-
-// type guards usando typeof
-
-let contador: number = 0
-
-function incrementarContador(cantidad: string | number) {
-    // contador += cantidad
-    // TypeScript nos daria un problema, debido a que no se puede utilizar el operador += con un string
-    if (typeof cantidad === "number") {
-        contador += cantidad
-    }
-    else if (typeof cantidad === "string" && !Number.isNaN(parseInt(cantidad))) { // Controlamos el tipo de dato y eliminamos un valor no deseado
-        const numberFromString: number = parseInt(cantidad)
-        contador += numberFromString
-    } else {
-        throw new Error(`"${cantidad}" no es valido. Ingrese un numero!`)
-    }
-}
-
-incrementarContador(23)
-
-console.log({ contador })
-
-incrementarContador("11")
-
-console.log({ contador })
-
-// incrementarContador("Benito Antonio Martinez Ocasio")
-
-/** typeof puede devolver un total de 8 "strings"
- * string
- * number
- * bigint
- * boolean
- * symbol
- * undefined
- * object
- * function
+// Ejercicio 1: aplicar las tecnicas de narrowing a la siguiente funcion, sin modificar su entrada
+/** Descripcion: Tenemos una funcion que asigna valores a una lista que es una propiedad de un objeto, debemos:
+ * 
+ * a.- Mantener el comportamiento esperado: "agregar un topping, en caso de asignarse"
+ * a.- Prevenir el uso de una propiedad en caso de que el objeto no la tenga en ese momento
+ * b.- Prevenir la asignacion de un valor inexistente
+ * c.- Devolver siempre un objeto de tipo "Pastel"
  */
 
-// narrowing a truthy
-
-function imprimirTodoPorPantalla(input: string | string[] | null) {
-    if(input && typeof input === "object") { // los Arrays en JS son de tipo "object", al igual que null!
-        for (const s of input) {
-            console.log(s)
-        }
-    } else if (typeof input === "string") {
-        console.log(input)
-    }
+interface Pastel {
+    nombre: string,
+    toppings?: string[]
 }
 
-// podriamos envolver toda la funcion en un checkeo global a truthy, pero no nos permitiria manejar el caso a string vacio ("") correctamente.
+function agregarTopping(pastel: Pastel, nuevoTopping?: string | undefined): Pastel {
+    return { nombre: pastel.nombre, toppings: [...pastel.toppings, nuevoTopping] }
+}
 
-imprimirTodoPorPantalla(null)
+const cheesecake: Pastel = { nombre: "cheesecake" }
+
+console.log(agregarTopping(cheesecake))
