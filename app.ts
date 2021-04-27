3960f9795f53dc7ed30b8bf381524ed6c586f7dc
salvgrd <salvgrd@gmail.com>
2021-04-27 23:24:31 +0200

    [feat] added exercise for generic functions

--- app.ts
+++ app.ts
@@ -1,3 +1,19 @@
-const helloWorld = 'Hola Mundo!'
+/**
+ * Ejercicio: Crear una funcion generica que reciba un elemento con la propiedad "area" y que ademas:
+ * Calcule la potencia al cuadrado del area del elemento
+ * Devuelva un elemento del mismo tipo
+ */
 
-console.log(helloWorld)
+interface Cuadrado {
+    name: "cuadrado",
+    area: number
+}
+
+interface Triangulo {
+    name: "triangulo"
+    area: number
+}
+
+function cuadradoDelArea(fig) {
+    return fig.area ** 2
+}
