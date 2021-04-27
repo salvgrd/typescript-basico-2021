f981fdd053fd3fa0de439e52426bd79271092f37
salvgrd <salvgrd@gmail.com>
2021-04-28 01:44:51 +0200

    [feat] added modules examples

--- app.ts
+++ app.ts
@@ -1,3 +1,19 @@
-const helloWorld = 'Hola Mundo!'
+// Modulos y ficheros
 
-console.log(helloWorld)
+import { saludar } from './funciones.js'
+import fs = require("fs")
+
+saludar()
+
+let jsonSalida = { 
+    name: 'Mike',
+    age: 23, 
+    gender: 'Male',
+    department: 'English',
+    car: 'Honda' 
+};
+ 
+let data = JSON.stringify(jsonSalida, null, '\t');
+fs.writeFileSync('archivo-de-salida.json', data);
+
+console.log('\n', JSON.parse(fs.readFileSync('archivo-de-salida.json', 'utf-8')))
---
+++ funciones.js
@@ -0,0 +1,7 @@
+function saludar() {
+    console.log("Saludos desde otro archivo")
+}
+
+module.exports = {
+    saludar: saludar
+}
