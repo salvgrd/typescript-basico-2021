5d2f7ab3a601b7bbd7ac0dedb0f33b055dee6c38
salvgrd <salvgrd@gmail.com>
2021-05-05 14:39:13 +0200

    [feat] using classes to build cli tool

--- app.ts
+++ app.ts
@@ -7,24 +7,55 @@ import figlet from "figlet"
 // import { stringify, jsonParse } from './utils'
 // import fs = require('fs')
 
+/**
+ * Proyecto final: Crear una herramienta de CLI que permita exportar un JSON de tipo "Carrito", y que ademas:
+ * Nos permita cargar "credito"
+ * Nos permita agregar y quitar productos del carrito
+ * Nos permita utilizar el credito para pagar el carrito de compras
+ * Nos permita ver los productos disponibles y sus precios
+ */
+
 console.clear()
 
-console.log(
-    chalk.green(
-      figlet.textSync('MercaScript', { horizontalLayout: 'full' })
-    )
-  );
+class App {
+  credito: number = 0;
 
-async function prompOptions() {
+  constructor() {
+    this.start();
+  }
+
+  start() {
+    console.log(
+      chalk.green(
+        figlet.textSync('MercaScript', { horizontalLayout: 'full' })
+      )
+    );
+    this.promptOptions()
+  }
+
+  async promptOptions() {
     const answer = await inquirer.prompt({
         type: 'list',
-        name: 'Actions',
+        name: 'option',
         choices: [
             "choice 1",
             "choice 2"
         ]
     })
-    console.log(answer)
+    this.executeOption(answer)
+  }
+
+  executeOption(answer: { option: string }) {
+    switch (answer.option) {
+      case "choice 1":
+        console.log("hacer cosas")
+        break;
+      
+      case "choice 2":
+        console.log(`mostrar credito: ${this.credito}`);
+      break;
+    }
+  }
 }
 
-prompOptions()
+new App();
