28ce14e5a651e4ccb83fed4e8d5de964b3741776
salvgrd <salvgrd@gmail.com>
2021-05-10 01:24:40 +0200

    feat: added functionalities

--- app.ts
+++ app.ts
@@ -3,9 +3,9 @@
 import inquirer from "inquirer"
 import chalk = require("chalk")
 import figlet from "figlet"
-// import { Producto, Carrito } from './interfaces'
-// import { stringify, jsonParse } from './utils'
-// import fs = require('fs')
+import { Producto, Carrito } from './interfaces'
+import { stringify, jsonParse } from './utils'
+import fs = require('fs')
 
 /**
  * Proyecto final: Crear una herramienta de CLI que permita exportar un JSON de tipo "Carrito", y que ademas:
@@ -17,60 +17,184 @@ import figlet from "figlet"
 
 // npm run build && npm run local
 
-console.clear()
-
 class App {
   credito: number = 0;
 
+  readonly availableProducts: Producto[] = []
+  cart: Carrito = { productos: [], total: 0, cantidadProductos: 0 }
+  menuMessage = ''
+
+  readonly options = {
+      addCredit: "Agregar credito",
+      buyProducts: "Comprar Productos",
+      displayCart: "Ver Carrito",
+      pay: "Pagar",
+  }
+
+  get productsNames() {
+      return this.availableProducts.map( p => p.nombre)
+  }
+
   constructor() {
+    this.availableProducts = this.fetchProducts();
+    this.cart = this.fetchCart();
+
     this.start();
   }
 
   start() {
-    console.log(
-      chalk.green(
-        figlet.textSync('MercaScript', { horizontalLayout: 'full' })
-      )
-    );
-    this.promptOptions()
+    this.displayMainMenu()
   }
 
-  async promptOptions() {
-    console.log({ credito: this.credito })
+  fetchProducts() {
+    const productsStrings = fs.readFileSync("productos.json", "utf-8");
+    const products = jsonParse<Producto[]>(productsStrings)
+
+    return products
+  }
+
+  fetchCart() {
+    const cartString = fs.readFileSync("carrito.json", "utf-8");
+    const cart = jsonParse<Carrito>(cartString)
+
+    return cart
+  }
+
+  displayLogo() {
+    console.log(
+        chalk.green(
+          figlet.textSync('MercaScript', { horizontalLayout: 'full' })
+        )
+      );
+  }
+
+  async displayMainMenu() {
+    console.clear();
+    this.displayLogo();
+    console.log({ credito: this.credito }, this.menuMessage)
     const answer = await inquirer.prompt({
         type: 'list',
         name: 'option',
-        choices: [
-            "Agregar credito",
-            "choice 2"
-        ]
+        message: "Que desea hacer?",
+        choices: Object.values(this.options)
     })
     this.executeOption(answer)
   }
 
   executeOption(answer: { option: string }) {
+    this.menuMessage = '';
+    const { options } = this
+
     switch (answer.option) {
-      case "Agregar credito":
-        this.agregarCredito()
+      case options.addCredit:
+        this.addCredit()
         break;
       
-      case "choice 2":
-        console.log(`mostrar credito: ${this.credito}`);
-      break;
+      case options.buyProducts:
+        this.displayProducts();
+        break;
+
+      case options.displayCart:
+        this.displayCart();
+        break
+
+      case options.pay:
+        this.checkout();
+        break
     }
   }
   
-  async agregarCredito() {
-    const { creditoAdicional } = await inquirer.prompt([
+  async addCredit() {
+    console.clear();
+    const { additionalCredit } = await inquirer.prompt([
       {
         type: "number",
-        name: "creditoAdicional",
+        name: "additionalCredit",
         message: "Cuanto credito?",
         default: 0
       }
     ])
-    this.credito += creditoAdicional;
+    this.credito += additionalCredit;
+    this.displayMainMenu();
   }
+
+  async displayProducts() {
+    console.clear();
+
+    const { selectedProduct } = await inquirer.prompt({
+        type: 'list',
+        name: 'selectedProduct',
+        message: "Elija un producto",
+        choices: this.productsNames
+    })
+
+    this.addProductToCart(selectedProduct);
+    this.displayMainMenu();
+  }
+
+  async addProductToCart(selectedProductName: string) {
+    const selectedProduct = this.availableProducts.find( p => p.nombre === selectedProductName)
+    const updatedCartProducts: Producto[] = selectedProduct ? 
+        [...this.cart.productos, selectedProduct] : this.cart.productos;
+    
+    this.updateCart(updatedCartProducts);
+  }
+
+  updateCart(products: Producto[]) {
+    const updatedCart: Carrito = { 
+        productos: products, 
+        cantidadProductos: products.length, 
+        total: 0 
+    }
+
+    for (let p of products) {
+        updatedCart.total += p.precio
+    }
+
+    this.cart = updatedCart
+  }
+
+  async displayCart() {
+    console.clear();
+
+    console.log(this.cart)
+
+    const answer = await inquirer.prompt({
+        type: 'list',
+        name: 'pay',
+        message: 'Desea pagar?',
+        choices: [ 
+            "Sí", 
+            "No (Volver al menú)" 
+        ]
+    })
+
+    answer.pay === "Sí" ? this.checkout() : this.displayMainMenu();
+  }
+
+  checkout() {
+    console.clear()
+    
+    const message = this.credito >= this.cart.total ? 
+        chalk.blueBright("Gracias y vuelva pronto! Su cambio es: ") : 
+        chalk.redBright("Su credito es insuficiente.")
+
+    if (this.credito >= this.cart.total) {
+        this.displayLogo();
+        console.log(message, this.credito - this.cart.total)
+        this.exportCartJson();
+    } else {
+        this.menuMessage = message
+        this.displayMainMenu();
+    }
+  }
+
+  exportCartJson() {
+    const cartJson = stringify<Carrito>(this.cart, undefined, ' ');
+
+    fs.writeFileSync("carrito.json", cartJson)
+  }
+
 }
 
 new App();
